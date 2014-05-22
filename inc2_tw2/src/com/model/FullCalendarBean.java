c249c5e9270365b0ba17185a39a3cbdabc534800
mu <aa@bb.cc>
2014-05-22 20:49:09 +0800

    20140522

--- inc2_tw2/src/com/model/FullCalendarBean.java
+++ inc2_tw2/src/com/model/FullCalendarBean.java
@@ -1,51 +1,60 @@
 package com.model;
 
 public class FullCalendarBean {
-	
+
 	private String id = "";
 	private String start = "";
 	private String end = "";
 	private String title = "";
 	private String url = "";
-	private String eventText ="";
-		
+	private String eventText = "";
+
 	public String getEventText() {
 		return eventText;
 	}
+
 	public void setEventText(String eventText) {
 		this.eventText = eventText;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getStart() {
 		return start;
 	}
+
 	public void setStart(String start) {
 		this.start = start;
 	}
+
 	public String getEnd() {
 		return end;
 	}
+
 	public void setEnd(String end) {
 		this.end = end;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getUrl() {
 		return url;
 	}
+
 	public void setUrl(String url) {
 		this.url = url;
 	}
-	
-	
-	
-}	
+
+}
--- inc2_tw2/src/com/servlets/FullCalendarServlet.java
+++ inc2_tw2/src/com/servlets/FullCalendarServlet.java
@@ -21,8 +21,6 @@ import com.model.FullCalendarBean;
 import com.model.ServiceResourceAccept;
 
 public class FullCalendarServlet extends HttpServlet {
-	
-
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
@@ -32,39 +30,37 @@ public class FullCalendarServlet extends HttpServlet {
 			IOException {
 
 		String srdid = request.getParameter("srdid");
-		
+
 		System.out.println("FullCalendarServlet------srdid=" + srdid);
 
 		MySQLAccess db = new MySQLAccess();
 		List<ServiceResourceAccept> sraList = db.readServiceResourceAcceptListForFullCalendar(srdid);
 		List<FullCalendarBean> fcList = new ArrayList();
-		for(Iterator it = sraList.iterator();it.hasNext(); ){
+		for (Iterator it = sraList.iterator(); it.hasNext();) {
 			FullCalendarBean fcBean = new FullCalendarBean();
-			ServiceResourceAccept sraBean = (ServiceResourceAccept)it.next();
+			ServiceResourceAccept sraBean = (ServiceResourceAccept) it.next();
 			String start = sraBean.getNeed_date_start();
 			String end = sraBean.getNeed_date_end();
 			fcBean.setId(sraBean.getSeq());
 			fcBean.setStart(start);
 			fcBean.setEnd(end);
 			fcBean.setTitle(sraBean.getCompany_name());
-			
+
 			SimpleDateFormat sdfParse = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-			SimpleDateFormat sdfFormat = new SimpleDateFormat("HH点mm分");			
+			SimpleDateFormat sdfFormat = new SimpleDateFormat("HH点mm分");
 			Date tempStart = null;
 			Date tempEnd = null;
-			
-			
+
 			try {
 				tempStart = sdfParse.parse(start);
 				tempEnd = sdfParse.parse(end);
-				fcBean.setEventText("需求方："+sraBean.getCompany_name()+"<br />"+
-						"需求时间："+	sdfFormat.format(tempStart)+" 至 "+
-						sdfFormat.format(tempEnd));
+				fcBean.setEventText("需求方：" + sraBean.getCompany_name() + "<br />" + "需求时间："
+						+ sdfFormat.format(tempStart) + " 至 " + sdfFormat.format(tempEnd));
 			} catch (ParseException e) {
-				
+
 				e.printStackTrace();
-			} 			
-				System.out.println("start to end  bean;;;;;");
+			}
+			System.out.println("start to end  bean;;;;;");
 			fcList.add(fcBean);
 		}
 		PrintWriter out = response.getWriter();
@@ -81,18 +77,16 @@ public class FullCalendarServlet extends HttpServlet {
 		JsonObject myObj = new JsonObject();
 		String jsonStr = "";
 		// ---------------------------------------------------------
-	
-			Gson gson = new Gson();
-			jsonStr = gson.toJson(fcList);
+
+		Gson gson = new Gson();
+		jsonStr = gson.toJson(fcList);
 
 		// ---------------------------------------------------------
 		out.println(jsonStr);
-		
+
 		System.out.println("FullCalendarServlet,,,   sraList.toString()=" + jsonStr);
-		
+
 		out.close();
 	}
-
-	
 
 }
